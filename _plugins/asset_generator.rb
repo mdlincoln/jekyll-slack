e82a3a8c0b8c19e97cc844e612623fc2541b1f9d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-11-23 15:55:41 -0500

    Create asset_generator to download Slack files

--- _plugins/asset_generator.rb
+++ _plugins/asset_generator.rb
@@ -1,13 +1,24 @@
+require "open-uri"
+
 module Jekyll
 
   class AssetGenerator < Generator
     safe true
 
     def generate(site)
-      files = @contexts.site.glob('_data/*').select { |f| File.directory? f }
-      channels.each do |channel|
-        channel_name = File.basename(channel)
-        site.pages << ChannelPage.new(site, site.source, channel_name, channel_name)
+      Dir.glob('_data/**/*.json') do |j|
+        messages = JSON.parse(File.read(j))
+        # Find every message that shares a file
+        attachment = messages.find {|msg| msg["subtype"] == "file_share"}
+        next if attachment.nil?
+        # Download the file to the assets/attachments/ directory
+        url = attachment["file"]["url_download"]
+        next if url.nil?
+        FileUtils.mkdir_p("assets/attachments")
+        filename = "assets/attachments/#{attachment['file']['id']}.#{attachment['file']['filetype']}"
+        open(filename, "wb") do |f|
+          f << open(url).read
+        end
       end
     end
   end
