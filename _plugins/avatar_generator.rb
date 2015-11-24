0fbb807124bd815b15d3ffb71085a5d0c2b4d857
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-11-23 23:48:34 -0500

    download user avatars on build

---
+++ _plugins/avatar_generator.rb
@@ -0,0 +1,22 @@
+require "open-uri"
+
+module Jekyll
+
+  class AvatarGenerator < Generator
+    safe true
+
+    def generate(site)
+      profiles = JSON.parse(File.read("_data/users.json"))
+      FileUtils.mkdir_p("assets/avatars")
+      profiles.each do |p|
+        user_id = p["id"]
+        img_url = p["profile"]["image_32"]
+        filename = "assets/avatars/#{user_id}.png"
+        open(filename, "wb") do |f|
+          f << open(img_url).read
+        end
+      end
+    end
+  end
+
+end
